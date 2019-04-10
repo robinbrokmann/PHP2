72d237710d90760ecb371f854f95357d448a2669
robin <106778@student.drenthecollege.nl>
2019-04-10 13:54:20 +0200

    0oefenopdracht 6 klaar

--- PHP2/Oefenopdrachten/oefening 6.php
+++ PHP2/Oefenopdrachten/oefening 6.php
@@ -1,3 +1,19 @@
 <?php
-$countchar=
+function countchar($string, $char) {
+
+
+    $counter = 0;
+
+    for($x = 0; $x < strlen($string); $x++) {
+
+        if ($string[$x] == $char) {
+            $counter++;
+        }
+    };
+
+    return $counter;
+}
+echo "het aantal opgevraagde letters:   ";
+echo countchar("dit is een zin gebruikt als een voorbeeld","e");
+
 ?>
