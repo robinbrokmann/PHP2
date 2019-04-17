01573b0890cdfe6b338c1ea9ed2f64cf2ce19c83
robin <106778@student.drenthecollege.nl>
2019-04-17 14:32:22 +0200

    Opdracht C comments in de code gezet

--- PHP2/Eindopdrachten/C/Opdracht C.php
+++ PHP2/Eindopdrachten/C/Opdracht C.php
@@ -1,13 +1,13 @@
 <?php
 
 function Fibonacci($nummer){
-
+// genereert de eerste twe nummers
     if ($nummer == 0)
         return 0;
     else if ($nummer == 1)
         return 1;
 
-    // mehtode om de volgende nummers op te halen
+    // methode om de volgende nummers op te halen
 
     else
         return (Fibonacci($nummer-1) +
@@ -16,6 +16,6 @@ function Fibonacci($nummer){
 
 $nummer = 10;
 for ($counter = 0; $counter < $nummer; $counter++){
-    echo Fibonacci($counter),' ';
+    echo Fibonacci($counter),'<br>';
 }
 ?>
